a736a41c583efe9341dca6f3320c3d18f7db2d4a
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-04-09 11:46:33 +0200

    Added basic html and js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// JavaScript
+ml5.setBackend("webgl");
+const nn = ml5.neuralNetwork({ task: 'classification', debug: true })
+const modelDetails = {
+    model: 'model/model.json',
+    metadata: 'model/model_meta.json',
+    weights: 'model/model.weights.bin'
+}
+nn.load(modelDetails, () => console.log("het model is geladen!"))
